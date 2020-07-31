04eb0b56ee66f7584b0208c2db7dd1e8da71761d
kopralhejo <greenlines99@gmail.com>
2020-07-31 20:05:44 +0700

    tambah lihat,delete,update mahasiswa (belom fix)

---
+++ app/src/main/java/com/example/catatanbimbingan/LihatBiodataActivity.java
@@ -0,0 +1,65 @@
+package com.example.catatanbimbingan;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class LihatBiodataActivity extends AppCompatActivity {
+    protected Cursor cursor;
+    DataHelper dbHelper;
+    Button ton2;
+    TextView text1, text2, text3, text4, text5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_biodata);
+        dbHelper = new DataHelper(this);
+        text1 = (TextView) findViewById(R.id.textView1);
+        text2 = (TextView) findViewById(R.id.textView2);
+        text3 = (TextView) findViewById(R.id.textView3);
+        text4 = (TextView) findViewById(R.id.textView4);
+        text5 = (TextView) findViewById(R.id.textView5);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM Mahasiswa WHERE nama = '" +
+                getIntent().getStringExtra("nama") + "'",null);
+        cursor.moveToFirst();
+        if (cursor.getCount()>0)
+        {
+            cursor.moveToPosition(0);
+            text1.setText(cursor.getString(0).toString());
+            text2.setText(cursor.getString(1).toString());
+            text3.setText(cursor.getString(2).toString());
+            text4.setText(cursor.getString(3).toString());
+            text5.setText(cursor.getString(4).toString());
+        }
+        ton2 = (Button) findViewById(R.id.button1);
+        ton2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/catatanbimbingan/MainActivity.java
+++ app/src/main/java/com/example/catatanbimbingan/MainActivity.java
@@ -77,11 +77,41 @@ public class MainActivity extends AppCompatActivity {
         cursor.moveToFirst();
         for (int cc = 0; cc < cursor.getCount(); cc++) {
             cursor.moveToPosition(cc);
-            daftar[cc] = cursor.getString(5).toString();
+            daftar[cc] = cursor.getString(3).toString();
         }
         ListView01 = (ListView) findViewById(R.id.listView1);
         ListView01.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
         ListView01.setSelected(true);
+        ListView01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView arg0, View arg1, int arg2, long arg3) {
+                final String selection = daftar[arg2]; //.getItemAtPosition(arg2).toString();
+                final CharSequence[] dialogitem = {"Lihat Biodata", "Update Biodata", "Hapus Biodata"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Pilihan");
+                builder.setItems(dialogitem, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        switch (item) {
+                            case 0:
+                                Intent i = new Intent(getApplicationContext(), LihatBiodataActivity.class);
+                                i.putExtra("nama", selection);
+                                startActivity(i);
+                                break;
+                            case 1:
+                                Intent in = new Intent(getApplicationContext(), LihatBiodataActivity.class);
+                                in.putExtra("nama", selection);
+                                startActivity(in);
+                                break;
+                            case 2:
+                                SQLiteDatabase db = dbcenter.getWritableDatabase();
+                                db.execSQL("delete from Mahasiswa where nama = '" + selection + "'");
+                                RefreshList();
+                                break;
+                        }
+                    }
+                });
+                builder.create().show();
+            }
+        });
         ((ArrayAdapter) ListView01.getAdapter()).notifyDataSetInvalidated();
     }
 }
